84b895c358d97f2fc8c077096fb56ff3882051f3
Matt Pindell <pindell.m@gmail.com>
2015-12-16 14:59:27 -0700

    Added SimpleCov

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -109,20 +109,3 @@ class EnigmaTest < Minitest::Test
   end
 
 end
-
-# require './lib/enigma'
-# > e = Enigma.new
-# > my_message = "this is so secret ..end.."
-# > output = e.encrypt(my_message)
-# => # encrypted message here
-# > output = e.encrypt(my_message, 12345, Date.today) #key and date are optional (gen random key and use today's date)
-# => # encrypted message here
-# > e.decrypt(output, 12345, Date.today)
-# => "this is so secret ..end.."
-# > e.decrypt(output, 12345) # Date is optional (use today's date)
-# => "this is so secret ..end.."
-
-# > e.crack(output, Date.today)
-# => "this is so secret ..end.."
-# > e.crack(output) # Date is optional, use today's date
-# => "this is so secret ..end.."
